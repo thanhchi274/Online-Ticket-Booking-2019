d9f5e55410ab0b0b29c3057535b8772b40a92f41
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2020-02-12 21:11:12 +0700

    redo detail movie

--- fe33finalproject/src/redux/action/index.js
+++ fe33finalproject/src/redux/action/index.js
@@ -64,6 +64,7 @@ export const actGetDetailMovieAPI = id => {
       url: `http://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayThongTinPhim?MaPhim=${id}`
     })
       .then(async result => {
+        console.log(result.data);
         dispatch({
           type: await ActionTypes.GET_DETAIL_MOVIE,
           movie: await result.data
@@ -213,7 +214,7 @@ export const actLayThongTinUser = user => {
     Axios({
       method: "POST",
       url:
-      "http://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/ThongTinTaiKhoan",
+        "http://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/ThongTinTaiKhoan",
       data: user
     })
       .then(async result => {
