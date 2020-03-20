c2091c54139c5315d3954d936ab5056054db1e50
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2020-03-20 13:09:22 +0700

    valid password type

--- src/pages/Home/signup.js
+++ src/pages/Home/signup.js
@@ -5,7 +5,8 @@ import {
   faLock,
   faPhone,
   faEnvelope,
-  faGlobe
+  faGlobe,
+  faCheck
 } from "@fortawesome/free-solid-svg-icons";
 import { connect } from "react-redux";
 import * as action from "../../Store/action";
@@ -13,94 +14,114 @@ import Success from "../../Components/success";
 import Fail from "../../Components/fail";
 
 const SignUpImage = props => (
-  <div className=" signup-img col-sm-6">
+  <div className=" signup-img">
     <img
       alt="hinhAnh123"
       src="https://cdn.jotfor.ms/images/podo-login-signup.png"
     />
-    <p className="desktop">{props.noti}</p>
+    <p>{props.noti}</p>
   </div>
 );
 
 const FormSignUp = props => (
   <form onSubmit={props.handleSubmit}>
-    <div className="row"></div>
-    <div className="input-div signup">
-      <div className="i">
-        <FontAwesomeIcon icon={faGlobe} />
+    <div className="form-wrapper">
+      <div className="signup-formLeft">
+        <div className="input-div signup">
+          <div className="i">
+            <FontAwesomeIcon icon={faGlobe} />
+          </div>
+          <div className="input-user">
+            <input
+              type="text"
+              className="input"
+              name="taiKhoan"
+              placeholder="Nhập UserName"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+            />
+          </div>
+        </div>
+        <div className="input-div signup">
+          <div className="i ">
+            <FontAwesomeIcon icon={faLock} />
+          </div>
+          <div>
+            <input
+              className="input"
+              type="password"
+              name="matKhau"
+              placeholder="Nhập Mật Khẩu"
+              onChange={props.handleChangeMatKhau}
+              onBlur={props.handleBlur}
+            />
+          </div>
+        </div>
+
+        <div className="passwordCheck">
+          <div className={props.count}>
+            <FontAwesomeIcon icon={faCheck} />
+            <p>Mật khẩu có ít nhất 4 kí tự</p>
+          </div>
+          <div className={props.upper}>
+            <FontAwesomeIcon icon={faCheck} />
+            <p>Mật khẩu có ít nhất 1 kí tự in hoa</p>
+          </div>
+          <div className={props.number}>
+            <FontAwesomeIcon icon={faCheck} />
+            <p>Mật khẩu có ít nhất 1 số</p>
+          </div>
+        </div>
       </div>
-      <div className="input-user">
-        <input
-          type="text"
-          className="input"
-          name="taiKhoan"
-          placeholder="Nhập UserName"
-          onChange={props.handleChange}
-          onBlur={props.handleBlur}
-        />
+      <div className="signup-formRight">
+        <div className="input-div signup">
+          <div className="i">
+            <FontAwesomeIcon icon={faUser} />
+          </div>
+          <div className="input-user">
+            <input
+              type="text"
+              className="input"
+              name="hoTen"
+              placeholder="Nhập đầy đủ họ tên"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+            />
+          </div>
+        </div>
+        <div className="input-div signup">
+          <div className="i">
+            <FontAwesomeIcon icon={faPhone} />
+          </div>
+          <div className="input-user">
+            <input
+              type="text"
+              className="input"
+              name="soDT"
+              placeholder="Nhập số điện thoại"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+            />
+          </div>
+        </div>
+        <div className="input-div signup">
+          <div className="i">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+          <div className="input-user">
+            <input
+              type="text"
+              className="input"
+              name="email"
+              placeholder="Nhập email"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+            />
+          </div>
+        </div>
       </div>
     </div>
-    <div className="input-div signup">
-      <div className="i ">
-        <FontAwesomeIcon icon={faLock} />
-      </div>
-      <div>
-        <input
-          className="input"
-          type="password"
-          name="matKhau"
-          placeholder="Nhập Mật Khẩu"
-          onChange={props.handleChange}
-          onBlur={props.handleBlur}
-        />
-      </div>
-    </div>
-    <div className="input-div signup">
-      <div className="i">
-        <FontAwesomeIcon icon={faUser} />
-      </div>
-      <div className="input-user">
-        <input
-          type="text"
-          className="input"
-          name="hoTen"
-          placeholder="Nhập đầy đủ họ tên"
-          onChange={props.handleChange}
-          onBlur={props.handleBlur}
-        />
-      </div>
-    </div>
-    <div className="input-div signup">
-      <div className="i">
-        <FontAwesomeIcon icon={faPhone} />
-      </div>
-      <div className="input-user">
-        <input
-          type="text"
-          className="input"
-          name="soDT"
-          placeholder="Nhập số điện thoại"
-          onChange={props.handleChange}
-          onBlur={props.handleBlur}
-        />
-      </div>
-    </div>
-    <div className="input-div signup">
-      <div className="i">
-        <FontAwesomeIcon icon={faEnvelope} />
-      </div>
-      <div className="input-user">
-        <input
-          type="text"
-          className="input"
-          name="email"
-          placeholder="Nhập email"
-          onChange={props.handleChange}
-          onBlur={props.handleBlur}
-        />
-      </div>
-    </div>
-    <p className="mobile mobile_noti">{props.noti}</p>
+    <p className="mobile_noti">{props.noti}</p>
     <button className="btn signup-btn" onClick={props.errorData}>
       SIGN UP
     </button>
@@ -129,7 +150,10 @@ class Signup extends Component {
       dtValid: false,
       emailValid: false,
       noti: "Chào bạn, điền đầy đủ thông tin vào form nhé",
-      fail: false
+      fail: false,
+      count: "checkCount",
+      upper: "checkUppercase",
+      number: "checkNumber"
     };
   }
 
@@ -203,17 +227,80 @@ class Signup extends Component {
       [name]: value
     });
   };
+  handleChange = e => {
+    let target = e.target;
+    let name = target.name;
+    let value = target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+  handleChangeMatKhau = e => {
+    let target = e.target;
+    let value = target.value;
+    let checkUpper = "(?=.*[A-Z])";
+    let checkNumber = "[0-9]";
+    this.setState(
+      {
+        matKhau: value
+      },
+      () => {
+        this.state.matKhau.length >= 4
+          ? this.setState({
+              count: "checkCount checked"
+            })
+          : this.setState({
+              count: "checkCount"
+            });
+        if (this.state.matKhau.match(checkUpper)) {
+          this.setState({
+            upper: "checkUppercase checked"
+          });
+        } else {
+          this.setState({
+            upper: "checkUppercase"
+          });
+        }
+        if (this.state.matKhau.match(checkNumber)) {
+          this.setState({
+            number: "checkNumber checked"
+          });
+        } else {
+          this.setState({
+            number: "checkNumber"
+          });
+        }
+      }
+    );
+  };
   handleSubmit = e => {
     e.preventDefault();
     let user = { ...this.state };
     let { tkValid, mkValid, dtValid, tenValid, emailValid } = this.state;
     user.maNhom = "GP01";
     user.maLoaiNguoiDung = "KhachHang";
-    if ((tkValid, mkValid, dtValid, tenValid, emailValid === true)) {
+    if (
+      (tkValid,
+      mkValid,
+      dtValid,
+      tenValid,
+      emailValid === true &&
+        this.state.upper === "checkUppercase checked" &&
+        this.state.count === "checkCount checked" &&
+        this.state.number === "checkNumber checked")
+    ) {
       this.props.signup(user, this.props.history);
     } else if ((tkValid, mkValid, dtValid, tenValid, emailValid === false)) {
       this.setState({
         noti: "Vui lòng điền đầy đủ thông tin"
+      });
+    } else if (
+      this.state.upper === "checkUppercase" ||
+      this.state.count === "checkCount" ||
+      this.state.number === "checkNumber"
+    ) {
+      this.setState({
+        noti: "Vui lòng điền đúng định dạng mật khẩu"
       });
     }
   };
@@ -227,26 +314,36 @@ class Signup extends Component {
       fail: false
     });
   };
+  renderError = signuped => {
+    return this.state.fail === true &&
+      this.props.errorSignup &&
+      signuped !== 200 ? (
+      <Fail tab={this.props.errorSignup} fail={this.toggleError} />
+    ) : null;
+  };
+  renderSuccess = signuped => {
+    return signuped === 200 ? <Success tab={"Đăng ký"} /> : null;
+  };
   renderHTML = () => {
     return (
       <div>
-        {this.state.fail === true && this.props.errorSignup ? (
-          <Fail tab={this.props.errorSignup} fail={this.toggleError} />
-        ) : null}
-        {this.props.signuped === 200 ? <Success tab={"Đăng ký"} /> : null}
+        {this.renderSuccess(this.props.signuped)}
+
+        {this.renderError(this.props.signuped)}
         <div className="signup-container">
-          <div className="signup-content row align-items-center">
-            <SignUpImage
-              className="desktop"
-              noti={this.state.noti}
-            ></SignUpImage>
-            <div className="signUp-form col-sm-6">
+          <div className="signup-content align-items-center">
+            <SignUpImage noti={this.state.noti}></SignUpImage>
+            <div className="signUp-form">
               <h4>Đăng ký</h4>
               <FormSignUp
                 errorData={this.handleError}
                 handleSubmit={this.handleSubmit}
                 handleChange={this.handleChange}
+                handleChangeMatKhau={this.handleChangeMatKhau}
                 noti={this.state.noti}
+                count={this.state.count}
+                upper={this.state.upper}
+                number={this.state.number}
                 handleBlur={this.handleBlur}
               ></FormSignUp>
             </div>
