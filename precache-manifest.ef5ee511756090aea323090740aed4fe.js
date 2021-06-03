c093034f4e96b88aa955bb243630e7bae03af379
thanhchi274 <anarapham274@gmail.com>
2021-06-03 07:41:24 +0700

    Updates

---
+++ precache-manifest.ef5ee511756090aea323090740aed4fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bec4da8a014de134c1eea74a37d764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388448341ae46b998f2f",
+    "url": "/static/css/2.ab1f0a18.chunk.css"
+  },
+  {
+    "revision": "45c041ec6da1ccf24a83",
+    "url": "/static/css/main.d36f17e0.chunk.css"
+  },
+  {
+    "revision": "388448341ae46b998f2f",
+    "url": "/static/js/2.0947f319.chunk.js"
+  },
+  {
+    "revision": "70a1b31db868c313365cf2bbbd928d00",
+    "url": "/static/js/2.0947f319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c041ec6da1ccf24a83",
+    "url": "/static/js/main.a66254d6.chunk.js"
+  },
+  {
+    "revision": "d60cb30eba6f3eca222f",
+    "url": "/static/js/runtime-main.cc5a405d.js"
+  },
+  {
+    "revision": "7254658921376bb84df465cb70a89b2a",
+    "url": "/static/media/black_panther-carousel.72546589.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Online-Ticket-Booking-2019/precache-manifest.e14984bbb367392582439c3a2d8d13b2.js"
+  "/precache-manifest.ef5ee511756090aea323090740aed4fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Online-Ticket-Booking-2019/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
