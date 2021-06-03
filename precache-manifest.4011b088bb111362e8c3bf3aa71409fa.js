affc2bd79e648c92c31cddf9160c8c348e1a30bf
thanhchi274 <anarapham274@gmail.com>
2021-06-03 07:45:43 +0700

    Updates

---
+++ precache-manifest.4011b088bb111362e8c3bf3aa71409fa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f0dd28498cf55a20f726a493071726",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/index.html"
+  },
+  {
+    "revision": "c0e95ec652c7ce1e8c97",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/css/2.c1f6b4f3.chunk.css"
+  },
+  {
+    "revision": "cf58a321d6fb72ab1d07",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/css/main.bf5dd3ea.chunk.css"
+  },
+  {
+    "revision": "c0e95ec652c7ce1e8c97",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/js/2.d5b2f818.chunk.js"
+  },
+  {
+    "revision": "70a1b31db868c313365cf2bbbd928d00",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/js/2.d5b2f818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf58a321d6fb72ab1d07",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/js/main.c79c6f3c.chunk.js"
+  },
+  {
+    "revision": "60c263b649a3ee82fb07",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/js/runtime-main.97c9e854.js"
+  },
+  {
+    "revision": "7254658921376bb84df465cb70a89b2a",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/media/black_panther-carousel.72546589.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Online-Ticket-Booking-2019/FinalProjectFE33BT/static/media/slick.f97e3bbf.svg"
+  }
+]);
